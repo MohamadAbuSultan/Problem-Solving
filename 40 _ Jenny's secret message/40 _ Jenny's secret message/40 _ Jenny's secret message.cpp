da6c0226c93aa6596b7559e6aaf9d41edf9f2927
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-05-28 10:11:32 +0300

    .

--- 40 _ Jenny's secret message/40 _ Jenny's secret message/40 _ Jenny's secret message.cpp
+++ 40 _ Jenny's secret message/40 _ Jenny's secret message/40 _ Jenny's secret message.cpp
@@ -3,13 +3,14 @@
 
 std::string greet(std::string name)
 {
-    if (name == "Johnny") {
-        return "Hello, my love!";
-    }
-    return "Hello, " + name + "!";
+    //if (name == "Johnny") {
+    //    return "Hello, my love!";
+    //}
+    return "Hello, " + (name == "Johnny" ? " my love" : name) + "!";
 }
 
 int main()
 {
-    std::cout << greet("Mohamad");
+    std::cout << greet("Mohamad") << std::endl;
+    std::cout << greet("Johnny");
 }
