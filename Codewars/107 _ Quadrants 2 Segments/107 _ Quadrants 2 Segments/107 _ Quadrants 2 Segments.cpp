79eb68dd26b5a5ac531071ce8d52641f3d63fbf4
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-08-31 11:04:11 +0300

    إضافة الملف 107 _ Quadrants 2 Segments

---
+++ Codewars/107 _ Quadrants 2 Segments/107 _ Quadrants 2 Segments/107 _ Quadrants 2 Segments.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+struct coord {
+    int x;
+    int y;
+};
+
+bool quadrant_segment(const coord& A, const coord& B) {
+    return (A.x > 0) != (B.x > 0) || (A.y > 0) != (B.y > 0);
+}
+
+int main()
+{
+    cout << quadrant_segment({ 1, 4 }, { 9, 5 }) << endl;
+    cout << quadrant_segment({ -1, 4 }, { 9, 5 }) << endl;
+    cout << quadrant_segment({ 1, 4 }, { -9, 5 }) << endl;
+    cout << quadrant_segment({ -1, 4 }, { -9, 5 });
+}
