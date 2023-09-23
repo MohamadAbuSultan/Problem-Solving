5d38fd2c3e8d3eb8dc3812d345d69e8f8988f35d
MohamadKhaled <mohamadknsult2@gmail.com>
2023-09-23 21:19:53 +0300

    September-2023

---
+++ Assuit/A - Say Hello With C++/A - Say Hello With C++/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace A___Say_Hello_With_C__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name : ");
+            string name = Console.ReadLine();
+            Console.Write($"Hello, {name}");
+        }
+    }
+}
