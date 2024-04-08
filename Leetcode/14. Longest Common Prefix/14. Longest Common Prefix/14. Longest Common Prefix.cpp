d92a9c6298fc9d0a1d47667a875460d07f55349a
MohamadKhaled <mohamadknsult2@gmail.com>
2024-04-08 15:14:20 +0300

    April_2024

---
+++ Leetcode/14. Longest Common Prefix/14. Longest Common Prefix/14. Longest Common Prefix.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string LongestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "There is no common prefix among the input strings.";
+    string result = "";
+    for (int i = 0; i < strs[0].size(); i++)
+    {
+        char currentChar = strs[0][i];
+        for (int j = 1; j < strs.size(); j++)
+        {
+            if (strs[j][i] != currentChar)
+                return result;
+        }
+        result += currentChar;
+    }
+    return result;
+}
+
+int main() {
+    vector<string> str = { "flower", "flower", "flower" };
+    cout << LongestCommonPrefix(str);
+    return 0;
+}
