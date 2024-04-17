88641fc921c87a9f95f302847b3c73885150d8d7
MohamadKhaled <mohamadknsult2@gmail.com>
2024-04-17 03:56:15 +0200

    April_2024

--- Leetcode/20. Valid Parentheses/20. Valid Parentheses/20. Valid Parentheses.cpp
+++ Leetcode/20. Valid Parentheses/20. Valid Parentheses/20. Valid Parentheses.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 #include <stack>
+#include <unordered_map>
 
 using namespace std;
 
+
 bool isValid(string s) {
-    stack<char> st; // create an empty stack to store opening brackets
-    for (char c : s) { // loop through each character in the string
-        if (c == '(' || c == '{' || c == '[') { // if the character is an opening bracket
-            st.push(c); // push it onto the stack
-        }
-        else { // if the character is a closing bracket
-            if (st.empty() || // if the stack is empty or 
-                (c == ')' && st.top() != '(') || // the closing bracket doesn't match the corresponding opening bracket at the top of the stack
-                (c == '}' && st.top() != '{') ||
-                (c == ']' && st.top() != '[')) {
-                return false; // the string is not valid, so return false
+    stack<char> st;
+    unordered_map<char, char> brackets = { {')', '('}, {'}', '{'}, {']', '['} };
+
+    for (char c : s) {
+        if (brackets.count(c)) {
+            if (st.empty() || st.top() != brackets[c]) {
+                return false;
             }
-            st.pop(); // otherwise, pop the opening bracket from the stack
+            st.pop();
+        }
+        else {
+            st.push(c);
         }
     }
-    return st.empty(); // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
-    // so the string is valid, otherwise, there are unmatched opening brackets, so return false
+
+    return st.empty();
 }
+
 
 int main()
 {
-    string s = "()[]{}";
+    string s = "()[]{}{]";
+    string s1 = "()[]{}";
     cout << isValid(s);
+    cout << isValid(s1);
 }
