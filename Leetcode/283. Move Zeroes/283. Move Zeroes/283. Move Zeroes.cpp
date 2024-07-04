ca995d0c1ccb00d1bdd00d16631c0687f663037b
MohamadKhaled <mohamadknsult2@gmail.com>
2024-07-04 15:34:00 +0300

    July-2024

--- Leetcode/283. Move Zeroes/283. Move Zeroes/283. Move Zeroes.cpp
+++ Leetcode/283. Move Zeroes/283. Move Zeroes/283. Move Zeroes.cpp
@@ -5,10 +5,22 @@
 using namespace std;
 
 void moveZeroes(vector<int>& nums) {
+    int lastNonZeroFoundAt = 0;
 
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            swap(nums[lastNonZeroFoundAt], nums[i]);
+            lastNonZeroFoundAt++;
+        }
+    }
+
+    for (int num : nums) {
+        cout << num << " ";
+    }
 }
 
-int main()
-{
-
+int main() {
+    vector<int> nums = { 0, 1, 0, 2, 3, 4, 5, 0, 5, 0 , 6 , 5 };
+    moveZeroes(nums);
+    return 0;
 }
