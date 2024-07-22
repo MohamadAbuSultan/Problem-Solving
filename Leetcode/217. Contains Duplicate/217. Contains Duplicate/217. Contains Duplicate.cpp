00fccaa48926b183db5737d6349b8ab1ef3a2098
MohamadKhaled <mohamadknsult2@gmail.com>
2024-07-22 07:04:48 +0300

    July_2024

---
+++ Leetcode/217. Contains Duplicate/217. Contains Duplicate/217. Contains Duplicate.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool containsDuplicate(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
+	for (int i = 1; i < nums.size(); i++)
+		if (nums[i] == nums[i - 1])
+			return true;
+	return false;
+}
+
+int main()
+{
+	vector<int> nums = { 1,1,2,3,4,5 };
+	cout << containsDuplicate(nums);
+}
